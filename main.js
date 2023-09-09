669bc67f31ca2a3042b00152953419e841dc9b72
Eitan Le Bras <eitanlebras@Ronans-MacBook-Pro.local>
2023-09-09 15:18:56 -0700

    formspree form

--- main.js
+++ main.js
@@ -119,15 +119,3 @@ sr.reveal(`.home__social`, {delay: 600})
 sr.reveal(`.about__img, .contact__box`,{origin: 'left'})
 sr.reveal(`.about__data, .contact__form`,{origin: 'right'})
 sr.reveal(`.steps__card, .product__card, .questions__group, .footer`,{interval: 100})
-
-
-const confetti__button = document.querySelector('#confetti__button');
-const canvas = document.querySelector('#confetti');
-
-const jsConfetti = new JSConfetti();
-
-confetti__button.addEventListener('onclick', () => {
-    alert("Message submitted successfully!");
-})
-
-
